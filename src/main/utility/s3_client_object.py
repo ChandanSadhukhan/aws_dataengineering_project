3579ab2657ea70ac6fbc2084d824d29f20ce01ce
ChandanSadhukhan <38068607+ChandanSadhukhan@users.noreply.github.com>
2023-09-21 23:23:29 +0200

    Added the logic to coppy the file to the local folder.

--- src/main/utility/s3_client_object.py
+++ src/main/utility/s3_client_object.py
@@ -1,4 +1,5 @@
-from src.main.utility import logging_config
+import logging
+from src.main.utility.logging_config import *
 import boto3
 from src.main.utility import encrypt_decrypt
 from resource.dev import config
@@ -8,12 +9,13 @@ class S3ClientProvider:
     def __init__(self, aws_access_key=None, aws_secret_key=None):
         self.aws_access_key = aws_access_key
         self.aws_secret_key = aws_secret_key
-        logging_config.logger.info("got the decripted assess key")
+        logger.info("got the decripted assess key")
         self.session = boto3.Session(
             aws_access_key_id=self.aws_access_key,
             aws_secret_access_key=self.aws_secret_key
         )
         self.s3_client = self.session.client('s3')
+        logger.info("Logged in to Amazon S3 directory")
 
     def get_client(self):
         return self.s3_client
@@ -28,4 +30,5 @@ class S3ClientProvider:
 # aws_secret_key = encrypt_decrypt. \
 #     decrypt(config.aws_secret_key)
 # s3= S3ClientProvider(aws_access_key, aws_secret_key).get_client()
+
 
---
+++ src/main/utility/spark_session.py
@@ -0,0 +1,21 @@
+from pyspark.sql import *
+
+from src.main.utility.logging_config import logger
+
+
+def spark_session() :
+    session = SparkSession.builder.master("local[*]")\
+        .appName("Spark_Session")\
+        .config("spark.driver.extraClassPath", "/Users/debajnidas/Documents/Spark/mysql-connector.deb")\
+        .getOrCreate()
+
+    if session.getActiveSession() is not None :
+        logger.info(f"Spark Session is created:")
+        return session
+    else:
+        return None
+
+
+
+# test
+session = spark_session()
